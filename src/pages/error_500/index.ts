1ce0bb99434c21ab894f811df412a3105ee5ce73
Mikhail Stroganov <misha-stroganov@yandex.ru>
2023-03-07 14:46:41 +0300

    fixes

--- src/pages/error_500/index.ts
+++ src/pages/error_500/index.ts
@@ -7,27 +7,13 @@ import styles from './styles.scss';
 export class Error_500 extends Block {
     constructor() {
         super({}); 
-        // this.element?.setAttribute('id', 'app')
-        
-        // this.element?.classList.add('form_page__form-input')
+
     }
     init() {
         this.children.navigation = new Navigation();
 
     }
-    // init() {
-    //     this.children.input_login = new Input( {
-    //         id: 'some',
-    //         name: 'login'
-    //     }),
-    //     this.children.input_password = new Input( {
-    //         id: 'some',
-    //         name: 'password',
-    //         label:'>'
-    //     })
-    // }
     render() {
-        // console.log(styles)
         return this.compile(template, {...this.props, styles});
     }
 }
